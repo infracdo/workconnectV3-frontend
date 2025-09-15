71874ce8615c129c8a24c3b18de2f875ebecaf12
root <root@wc-telegraf-test.workconnect-system>
2025-09-15 14:15:08 +0800

    add backend url

--- quasar.config.js
+++ quasar.config.js
@@ -89,10 +89,10 @@ module.exports = configure(function (ctx) {
         VUE_APP_KEYCLOAK_CLIENT_ID: "apollo",
 
         VUE_APP_WORKCONNECT_API_URL: ctx.debug
-          ? "http://202.60.10.144:7500"
+          ? "http://192.168.61.42:7500"
           : ctx.prod
-          ? "https://workconnect.apolloglobal.net:7500"
-          : "http://202.60.10.144:7500",
+          ? "https://wcv2-be-test.am1-aks.apolloglobal.net:7500"
+          : "http://192.168.61.42:7500",
 
         VUE_APP_ELASTICSEARCH_URL: "http://192.168.43.60:5601/",
 
